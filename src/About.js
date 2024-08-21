969e709ffb81131fa6bb36d56258687bba38be9a
melvingonzalezjr <mgonzalez713@gmail.com>
2024-08-21 09:18:59 -0500

    Create event listener on My Why section

--- src/About.js
+++ src/About.js
@@ -1,18 +1,40 @@
-
+//Get ny Who am I statement
 const myWho = document.getElementById('myWho');
 let myWhoText = myWho.textContent
 
-myWho.addEventListener('mouseover', e => {
+//even handler functions to show answer to "Who am I?" or to revert to initial content.
+function revealMyWho(e) {
     e.target.textContent = 
 `Software Engineer in Space City!
 I'm interested in AI, Data Science, and Mathematics
 Learn more about me with above links`
     e.target.classList.add('myWhoMouseOver');
-});
-
-myWho.addEventListener('mouseout', e => {
+};
+function hideMyWho(e) {
     e.target.textContent = myWhoText
     e.target.classList.remove('myWhoMouseOver')
-});
+}
+//event listeners for when mouse is over div or leaves it.
+myWho.addEventListener('mouseover', revealMyWho);
+myWho.addEventListener('mouseout', hideMyWho);
 
+//querySelector to get the #whyContainer div and append a newly created h2 with textContent 'My Why'
+const myWhy = document.querySelector('#whyContainer');
+const h2Why = document.createElement('h2');
+h2Why.textContent = 'My Why';
+let myWhyText = h2Why.textContent;
+myWhy.appendChild(h2Why);
 
+//handler functions similar to those on 6 and 13. but now for My Why section
+function revealMyWhy(e) {
+    e.target.textContent = 
+    `The best contributions a person makes are not what they do but what they inspire and enable others to do. I believe in the power of Artificial Intelligence and Human Intelligence to accelerate creativity and innovation on a scale unimaginable today. I want to develop AI tools that free thinkers use to solve societal problems. I don't necessarily want to be a change-maker in the world; I want to be the catalyst for that change.`;
+    e.target.classList.add('myWhyMouseOver');
+};
+function hideMyWhy(e) {
+    e.target.textContent = myWhyText;
+    e.target.classList.remove('myWhyMouseOver')
+}
+
+myWhy.addEventListener('mouseover', revealMyWhy);
+myWhy.addEventListener('mouseout', hideMyWhy);
